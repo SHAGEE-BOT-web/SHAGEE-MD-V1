01ed79729581c908f1255c5e78c602d3a28ea4f1
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-08-29 21:26:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_MSG:  process.env.ALIVE_IMG || "https://telegra.ph/file/ec57b757c3c5890d510c3.jpg",
+ALIVE_MSG:  process.env.ALIVE_MSG || "> ⭕𝗛𝗘𝗟𝗟𝗢 𝗕𝗨𝗗𝗗𝗬 𝗜'𝗺 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪
+> ⭕𝗧𝗬𝗣𝗘 .𝗠𝗘𝗡𝗨 𝗔𝗟𝗟 𝗖𝗠𝗗 𝗟𝗜𝗦𝗧
+> σɯɳҽɾ ɳυɱႦҽɾ- 94717518710
+> 𝐒𝐇𝐀𝐆𝐄𝐄-𝐌𝐃 𝐕1",
 };
