0d23231cbe4690da7cbc4a4122ab23dc70640864
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-08-30 17:03:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_MSG:  process.env.ALIVE_IMG || "https://telegra.ph/file/ec57b757c3c5890d510c3.jpg",
 ALIVE_MSG:  process.env.ALIVE_MSG || "> ⭕𝗛𝗘𝗟𝗟𝗢 𝗕𝗨𝗗𝗗𝗬 𝐒𝐇𝐀𝐆𝐄𝐄-𝐌𝐃 ,𝗜'𝗺 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪 ",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE:process.env.MODE || "public",
 };
