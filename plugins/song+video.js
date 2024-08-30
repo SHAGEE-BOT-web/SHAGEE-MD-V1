f214ba093a3b3fc7e0f90fb5a11e8680e81f9c15
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-08-30 11:45:58 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -41,9 +41,64 @@ let downlodUrl = down.dl_url
 
 // send audio massge
 await conn.sendMassge(from,{audio:{url:downlodUrl},minetype:"audio/mpeg"},{quoted:mek})
-
+await conn.sendMassge(from,{document:{url:downlodUrl},minetype:"audio/mpeg",fileName:data.title +".mp3",caption:"DOWNLOD BY SHAGEE-MD V1"},{quoted:mek})
 
 }catch(e){
+console.log(e)
+ reply(`${e}`)
+}
+}}
+
+
+
+//===========================video-dl+document================
+
+
+cmd({
+    pattern: "video",
+    desc: "video downlod
+    category: "downlod",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if (q) retrun  reply("*🧑‍🔧Please give me a text or url that I want to search!🧑‍🔧*")
+const search = await  yts(q)
+const data =search .videos[0]:
+const url = data.url
+
+let desc =`🎶 *SHAGEE MD VIDEO DOWNLODER* 🎶  
+|__________
+|\_ ℹ️ 𝚃𝚒𝚝𝚕𝚎 : ${data.title}
+|
+|\_🗒️ Description : ${data.description}
+|
+|\_🕘 𝚃𝚒𝚖𝚎 : ${data.timestamp}
+|
+|\_📌 𝙰𝚐𝚘 :${data.ago}
+|
+|\_📉 𝚅𝚒𝚎𝚠𝚜 :${data.views}
+|__________
+
+*_DOWNLODING YOUR VIDEO..._*
+
+> POWERED by DINETH Ofc🚀📩
+> downlod by SHAGEE MD 
+`
+  await conn.sendMassge(from,{image:{url data.thumbnail},caption:desc},{quoted:mek}):
+
+  // downlod video
+
+let down = await fg.ytv(url)
+let downlodUrl = down.dl_url
+    
+// send video massge
+    
+await conn.sendMassge(from,{video:{url:downlodUrl},minetype:"video/mp4"},{quoted:mek})
+await conn.sendMassge(from,{document:{url:downlodUrl},minetype:"video/mp4",fileName:data.title +".mp4",caption:"*DOWNLOD BY SHAGEE-MD V1*"},{quoted:mek})
+
+
+
 console.log(e)
  reply(`${e}`)
 }
