fc4fa1c1f72613e8da532032576f7b7c45395988
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-08-30 16:18:58 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,12 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "system",
+  alias:["status"],
+    desc: "Check bot online or no.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
