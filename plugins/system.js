331f2d0d8dc95e61cb86f9ea86b55ca7e25db343
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-08-30 16:54:27 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,12 +1,32 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-
+const os = require("os")
 cmd({
     pattern: "system",
   alias:["status"],
-    desc: "Check bot online or no.",
+    desc: "Check bot uptime,ram usage and more....",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+let status = `┌───────────────────────
+├  ⏱️ Runtime:- ${runtime(process.uptime())}
+├ 🎞️ Ram usage:-${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+├ ⚙️  Platform:- ${os.hostname()}
+├ 👨‍💻 Owners:- SHAGEE
+├ 🧭 Version:- 1.0
+└───────────────────────
+
+> POWERED BY SHAGEE ofc
+`
+
+return reply(`${status}`)
+    
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+    
