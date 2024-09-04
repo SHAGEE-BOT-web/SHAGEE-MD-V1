b127109e1e6d7e43e1322af86bb776ce6f0795d0
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-09-04 21:56:41 +0530

    Create shagee.js

---
+++ plugins/shagee.js
@@ -0,0 +1,24 @@
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "shagee",
+    desc: "wallpaper the bot",
+    category: "main",
+    react: "☠",
+    filename: __filename
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let dec = `❮❮ MY KYLIE WALLPAPER 👾💗 ❯❯`
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/ec57b757c3c5890d510c3.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/ec57b757c3c5890d510c3.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/ec57b757c3c5890d510c3.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/ec57b757c3c5890d510c3.jpg`},caption:dec},{quoted:mek});
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
