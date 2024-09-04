6f906cdda8402f094952e474fd848363d3bc80b8
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-09-04 21:46:34 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,18 +1,20 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-const { fetchJson } = require ('../lib/functions')
+const { fetchJson } = require('../lib/functions')
+
 cmd({
     pattern: "ai",
-    desc: "ai chat",
+    desc: "ai",
     category: "main",
+    react: "👾",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
-return reply(`${data.data`)
+reply(`${data.data}`)
 }catch(e){
 console.log(e)
-reply(`{e}`)
+reply(`${e}`)
 }
-}}
+})
