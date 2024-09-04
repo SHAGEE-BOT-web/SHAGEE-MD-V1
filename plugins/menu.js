35c1c16c24572fa43223f455a25279d8003a1894
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-09-04 21:46:00 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,1 +1,71 @@
-hmmm
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "menu the bot",
+    category: "menu",
+    react: "📜",
+    filename: __filename
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+
+let dec = `*👋 Hello ${pushname}*
+
+*╭───────────◉◉►*
+*│Hey, I'm SHAGEE-MD, Created By*
+*│SHAGEE Tech✨🍂*
+*│*
+*│I'm always here for help you.😇 Love │you forever ♥️*
+*╰───────────◉◉►*
+
+*╭───────────◎◎►*
+│💻𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦💻
+*│ ─────────*
+*│►.song*
+*│►.video*
+*│►.fb*
+*│►.tt*
+*│►.mediafire*
+*│►.twitter*
+*│►.gdrive*
+*│►.apk*
+*│►.news*
+*│►.img*
+*│►.kylie*
+│─────────
+│👾𝗔𝗜 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦👾
+*│─────────*
+*│►.ai*
+│─────────
+│📍𝗢𝗪𝗡𝗘𝗥 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦📍
+*│─────────*
+*│►.restart*
+│─────────
+│⛥𝗚𝗥𝗢𝗨𝗣 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦⛥
+*│─────────*
+*│►.promote*
+*│►.demote*
+*│►.remove*
+*│►.setpic*
+*│►.del*
+*│►.add*
+*│►.setwelcome*
+*│►.setgoodbye*
+│ ─────────
+│💫𝗠𝗔𝗜𝗡 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦💫
+*│─────────*
+*│►.menu*
+*│►.alive*
+*│►.ping*
+*│►.repo*
+*│►.owner*
+*╰───────────◎◎►*
+*©SHAGEE-MD BY DINETH WISHMITHAッ*`
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/24ee445077f52ea56c4f3.jpg`},caption:dec},{quoted:mek});
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
