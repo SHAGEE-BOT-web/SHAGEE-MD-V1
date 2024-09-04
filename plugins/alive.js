9af89960ca677f308fc1c878f51472fb01d201ba
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-09-04 21:49:15 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,3 +1,5 @@
+
+
 const config = require('../config')
 const {cmd , commands} = require('../command')
 
@@ -5,15 +7,27 @@ cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
     category: "main",
+    react: "👋",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+
+let des = `*👋 Hello ${pushname}*
+
+*╔╭────────────╮╕*
+*╭│I'm Alive Now👾  │─◎◎▷*
+*╘╰────────────╯╜*
+*│A SHAGEE-MD │Whatsapp Bot Based │Many │Services With A │RealTime* *Automated │Consversational* *││Experience, Enjoy💫.*
+*| So,I Think This Bots Are Useful To You.📍*
+*│*
+*│Type .menu To Get Bot User*
+*│Menu💫*
+*╰───────────────◎◎▷*
+`
+return await conn.sendMessage(from,{image: {url: `FOTO EK`},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
-
