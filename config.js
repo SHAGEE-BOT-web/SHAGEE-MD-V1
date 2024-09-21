cbfe93f4739def64a90a3e90d3f5a1a2f588d9bf
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-09-21 12:37:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "AOdwVT4R#G0h7_4IqxaMhxWcwEW_dCSy6iZn8MvMPlmcAifvFVhk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ec57b757c3c5890d510c3.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi SHAGEE-MD Is Online Now 💻\n*💻 Owner* - SHAGEE-MD\n\n*💻 Owner Number* -94710136994",
 SUDO_NB: process.env.SUDO_NB || "94710136994",
