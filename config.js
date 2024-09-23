df5b6a9beee2e1f7c73145d6e6bbc8424f331293
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-09-23 15:34:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,6 @@ ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi SHAGEE-MD Is Online Now 💻\n*
 SUDO_NB: process.env.SUDO_NB || "94710136994",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+AUTO_VOICE:"true",
+MONGODB: process.env.MONGODB || "enter mongodb here",
 };
